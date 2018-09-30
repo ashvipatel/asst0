6d0b263a3dd7d850e208b1378b0c06fda597c420
cocoacitrus <bowman_snowman@me.com>
2018-09-30 13:02:43 -0400

    need to test append and print

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -5,38 +5,28 @@
 #include <ctype.h>
 #include "simpleCSVsorter.h"
 
-
+/* ***
+ * 	Global Variables
+ ****/
 
 int main(int argc, char** argv)
 {
-	//char* buffer;
+
+	list_of_records* head = (list_of_records*)malloc(sizeof(list_of_records));
 
 	// error check command line arguments
+	/*
 	if(argc != 3)
 	{
 		printf("Fatal Error in %s on line %d\nToo Few Arguments.\n", __FILE__, __LINE__);
 		return -1;
 	}
+	*/
 
-	//key_type = argv[2];
-	integer = TRUE;	
-
-	//  build a little record list to test append() and print_to_stdout()
-	//Record* head = (Record*)malloc(sizeof(Record));
-	Record* head = NULL;
-	//head -> next = NULL;
-
-	append(head, (void*)2, "cheese");
-//	print_to_stdout(head);
-	append(head, (void*)2, "dino");
-//	print_to_stdout(head);
-	append(head, (void*)2, "wonka");
-//	print_to_stdout(head);
-	append(head, (void*)2, "grape");
-//	print_to_stdout(head);
-	append(head, (void*)2, "error");
-	print_to_stdout(head);
-
+	// TESTING APPEND AND PRINT TO STDOUT
+	
+	free(head);
+	
 	return 1;
 }
 
--- simpleCSVsorter.h
+++ simpleCSVsorter.h
@@ -19,6 +19,12 @@ typedef struct _Record
 	struct _Record* next;
 } Record;
 
+typedef struct _list_of_records
+{
+	char* col_values;
+	Record* first;
+} list_of_records;
+
 
 
 /*****
@@ -43,10 +49,10 @@ boolean floating = FALSE;
  * 	Function Prototypes
  *
  ******/
-void append(Record*, void*, char*);
+void append(list_of_records*, void*, char*);
 //int clear(Record*);
 //int readCSV( STDIN, Record*, char*):
-void print_to_stdout(Record*);
+void print_to_stdout(list_of_records*);
 //int write_to_file(<file descriptor?>, Record*);
 
 
@@ -60,11 +66,11 @@ void print_to_stdout(Record*);
 /* ***
  * 	appends "key" to the end of the linked list beginning at "head"
  ****/
-void append(Record* head, void* k, char* comp)
+void append(list_of_records* head, void* k, char* comp)
 {
 printf("------ append ------\n");
 	// cast type of key before appending to list
-	if(integer == TRUE)
+	/*if(integer == TRUE)
 	{
 		k = (int*)k;
 	}
@@ -72,31 +78,45 @@ printf("------ append ------\n");
 		k = (float*)k;
 	else
 		k = (char*)k;
-
-	// if list is empty, initialize "head"
-	if(head == NULL)
+*/
+	// if list is empty, initialize first node
+	if(head->first == NULL)
 	{
 		Record* newNode = (Record*)malloc(sizeof(Record));
-		newNode->key = (void*)k;
+
+		if(integer==TRUE)
+			newNode->key = (int*)k;
+		else if(floating == TRUE)
+			newNode->key = (float*)k;
+		else
+			newNode->key = (char*)k;
+
 		newNode->complete = comp;
-		head = newNode;
-		//head->next = NULL;
+		head->first = newNode;
 		return;
 	}
 
-	Record* tmp = head->next;
+	// head->first has been initialized
+	Record* tmp = head->first;
 	
-	while(tmp != NULL)
+	while(tmp->next != NULL)
 	{
 		printf("tmp = %s -- ", tmp->complete);
 		tmp = tmp->next;
 	}
 	printf("\n");
+	// allocte mem for new node. insert appropriately casted k
 	Record* newNode = (Record*)malloc(sizeof(Record));
-	newNode->key = k;
+	if(integer==TRUE)
+		newNode->key = (int*)k;
+	else if(floating == TRUE)
+		newNode->key = (float*)k;
+	else
+		newNode->key = (char*)k;
+	// append new node to list
 	newNode->complete = comp;
-	newNode->next = NULL;
-	tmp = newNode;
+	//newNode->next = NULL;
+	tmp->next = newNode;
 }
 
 
@@ -105,14 +125,15 @@ printf("------ append ------\n");
 /* *** 					THIS FUNCTION IS TEMPORARY, TO TEST APPEND()
  * 	write the CSV to STDOUT
  ****/
-void print_to_stdout(Record* head)
+void print_to_stdout(list_of_records* head)
 {
-	printf("the problem has been with this fucking print function all along!\n");
-	Record* tmp = head->next;
+	if(head != NULL)
+		printf("%s\n", head->col_values);
+	else
+		printf("whoops\n");
+	Record* tmp = head->first;
 	while(tmp != NULL)
 	{
-printf("i give up\n");
-		printf("head != NULL\n");
 		printf("%s -->", tmp->complete);
 		tmp = tmp->next;
 	}
